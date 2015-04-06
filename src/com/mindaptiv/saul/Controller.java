42d7a276b74f955bf95b9e0b6776ba15b6e01d58
scrubsauce <josh@mindaptiv.com>
2015-04-06 14:07:21 -0600

    add handleKeyEvent()

--- src/com/mindaptiv/saul/Controller.java
+++ src/com/mindaptiv/saul/Controller.java
@@ -9,6 +9,7 @@ import java.lang.Integer;
 
 import android.os.Build;
 import android.view.InputDevice;
+import android.view.KeyEvent;
 
 public class Controller 
 {
@@ -27,6 +28,23 @@ public class Controller
 	Integer thumbLeftY;
 	Integer thumbRightX;
 	Integer thumbRightY;
+	Float   fLeftTrigger;
+	Float	fRightTrigger;
+	Float   fThumbLeftX;
+	Float	fThumbLeftY;
+	Float	fThumbRightX;
+	Float	fThumbRightY;
+	
+	//test
+	int keycode;
+	
+	//buttons mapping
+	/*
+	 * 1st Byte: (Right)(Left)(Down)(Up)
+	 * 2nd Byte: (Right Thumbstick)(Left Thumbstick)(Back)(Start)
+	 * 3rd Byte: (Don't Care)(Don't Care)(R1)(L1)
+	 * 4th Byte: (Y)(X)(B)(A)
+	 */
 
 	
 	//Constructor
@@ -45,6 +63,83 @@ public class Controller
 		{
 			this.userIndex = 0;
 		}
-	}
+		
+		//parse buttons
+		
+		
+		
+	}//END constructor
+	
+	public void handleKeyEvent(KeyEvent keyEvent)
+	{
+		//Variable declaration
+		int key = keyEvent.getKeyCode();
+		
+		//test
+		this.keycode = key;
+		
+		//parse keycode
+		if (key == KeyEvent.KEYCODE_BUTTON_A)
+		{
+			
+		}
+		else if (key == KeyEvent.KEYCODE_BUTTON_B)
+		{
+			
+		}
+		else if (key == KeyEvent.KEYCODE_BUTTON_X)
+		{
+			
+		}
+		else if (key == KeyEvent.KEYCODE_BUTTON_Y)
+		{
+			
+		}
+		else if (key == KeyEvent.KEYCODE_BUTTON_B)
+		{
+			
+		}
+		else if (key == KeyEvent.KEYCODE_BUTTON_L1)
+		{
+			
+		}
+		else if (key == KeyEvent.KEYCODE_BUTTON_R1)
+		{
+			
+		}
+		else if (key == KeyEvent.KEYCODE_BUTTON_START)
+		{
+			
+		}
+		else if (key == KeyEvent.KEYCODE_BUTTON_SELECT)
+		{
+			
+		}
+		else if (key == KeyEvent.KEYCODE_BUTTON_THUMBL)
+		{
+			
+		}
+		else if (key == KeyEvent.KEYCODE_BUTTON_THUMBR)
+		{
+			
+		}
+		else if (key == KeyEvent.KEYCODE_DPAD_UP)
+		{
+			
+		}
+		else if (key == KeyEvent.KEYCODE_DPAD_DOWN)
+		{
+			
+		}
+		else if (key == KeyEvent.KEYCODE_DPAD_LEFT)
+		{
+			
+		}
+		else if (key == KeyEvent.KEYCODE_DPAD_RIGHT)
+		{
+			
+		}
+	}//END handleKeyEvent()
+	
 
 }
